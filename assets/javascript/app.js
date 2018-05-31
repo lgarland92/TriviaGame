dc918156e84a4fcf87e27bd93bf7d8e60515deb5
Lindsey Garland <rebeccgaupo@gmail.com>
2018-05-31 15:09:03 -0700

    adding basics

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var rightAnswer= 0;
+var wrongAnswer= 0;
+
+var questions= ["1.	Which of these characters was NOT an original member of the Scooby Gang?","2. Where is the Hellmouth?", "3. In which episode do you see Xander ACTUALLY ride his skateboard?", "4. Which of these monsters does NOT appear in the series?", "5. What does MOO stand for?"];
+
+var q1= ["a. Willow", "b. Xander", "c.	Anya", "d. Buffy"];
+
+var q2= ["a. Under the library ", "b.	Under the Bronze", "c. Through a portal in Buffy’s room", "d. At your mom’s house"];
+
+var q3= ["a.	Season 4, Episode 5", "b.	Season 1, Episode 1 ", "c.	Season 6, Episode 2", "d.	Season 3, Episode 17"];
+
+var q4= ["a. Werewolf", "b.	Giant Praying Mantis Monster", "c.	Ifrit", "d.	Mummy"];
+
+var q5= ["a. The sound cows make", "b.	Morbid and Occult Occasions", "c.	Merely Obnoxious Operation", "d.	Mothers Opposed to the Occult"];
